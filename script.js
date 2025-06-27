c878076d293ad45e7348ec9e0d0dabed8ab177e8
Prakhar Pandey <prakharp4pandey@gmail.com>
2025-06-27 17:24:34 +0530

    fix

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-// Toggle start menu
-const startButton = document.getElementById("start-button");
-const startMenu = document.getElementById("start-menu");
-
-startButton.addEventListener("click", () => {
-  startMenu.classList.toggle("hidden");
-});
-
-// My Computer popup
-function openMyComputer() {
-  const windowDiv = document.createElement("div");
-  windowDiv.classList.add("window");
-  windowDiv.innerHTML = `
-    <div class="window-header">
-      <span>My Computer</span>
-      <button onclick="this.parentElement.parentElement.remove()">X</button>
-    </div>
-    <div class="window-content">
-      <p>This is a simulated 'My Computer' window!</p>
-    </div>
-  `;
-  document.body.appendChild(windowDiv);
+function toggleStartMenu() {
+  const menu = document.getElementById("startMenu");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 }
